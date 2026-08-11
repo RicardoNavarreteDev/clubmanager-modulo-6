513f02ce1cdaad7640286f1c527779c3878a9e3b
RicardoN <navarrete.soto90@gmail.com>
2026-08-11 17:54:45 -0400

    feat: agregar pagina 404 personalizada

--- src/app.js
+++ src/app.js
@@ -16,10 +16,19 @@ app.use(morgan('dev'));
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 
+// Servimos archivos publicos como CSS, imagenes o JS del navegador.
 app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', path.join(__dirname, 'views'));
 
+// Montamos la ruta principal en /.
 app.use("/", homeRouter);
+
+// Montamos las rutas de jugadores bajo /jugadores.
 app.use("/jugadores", playersRouter);
+
+// Si ninguna ruta coincide, respondemos con la vista 404.
+app.use((req, res) => {
+  res.status(404).render("404");
+});
 
 export default app;
